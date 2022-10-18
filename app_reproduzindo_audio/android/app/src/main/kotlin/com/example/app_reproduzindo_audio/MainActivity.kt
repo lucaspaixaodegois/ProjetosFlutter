dc4e7198d9d3d93581659e8ba29e8ef0fae0cfdd
Lucas Paixão de Gois <lucaspaixaodegois@gmail.com>
2022-10-18 16:22:35 -0300

    iniciando projeto reproduzir audio

---
+++ app_reproduzindo_audio/android/app/src/main/kotlin/com/example/app_reproduzindo_audio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_reproduzindo_audio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_reproduzindo_audio/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
